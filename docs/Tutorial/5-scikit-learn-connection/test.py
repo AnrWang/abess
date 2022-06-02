2345129ec0d22291f99daf7b453947b75568573d
AnrWang <wanganran@mail.ustc.edu.cn>
2022-06-02 19:48:00 +0800

    test

---
+++ docs/Tutorial/5-scikit-learn-connection/test.py
@@ -0,0 +1,25 @@
+"""
+Subject
+======================
+"""
+
+# %%
+#Some introduce
+#
+#
+
+###############################################################################
+# Subtitle
+# ---------------------
+# Some introduce
+#
+#
+
+print("This is a test.")
+
+# %%
+# 
+# 
+# 
+# sphinx_gallery_thumbnail_path = 'Tutorial/figure/test.png'
+# 
